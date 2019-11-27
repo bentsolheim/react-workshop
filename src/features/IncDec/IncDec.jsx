1f8e6c107df3366d56e2ff225933fd63c7ed56be
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2019-11-27 13:07:18 +0100

    Multiple comps

--- src/features/IncDec/IncDec.jsx
+++ src/features/IncDec/IncDec.jsx
@@ -1,22 +1,47 @@
 import React, { useState } from "react";
 import Button from "./Button";
+import PropTypes from "prop-types";
 
-const IncDec = () => {
-  const [count, setCount] = useState(0);
+const DisplayCounter = props => {
+  return <h1>{props.count}</h1>;
+};
+DisplayCounter.propTypes = {
+  count: PropTypes.number.isRequired,
+}
+
+const CountControls = (props) => {
+  const { count, onCountChange } = props
 
   const mut = m => {
     return () => {
-      setCount(count + m);
+      onCountChange(count + m);
     };
   };
   const onIncClick = mut(1);
   const onDecClick = mut(-1);
+  return (
+    <>
+      <Button onClick={onDecClick}>Dec</Button>
+      <Button onClick={onIncClick}>Inc</Button>
+    </>
+  );
+};
+CountControls.propTypes = {
+  count: PropTypes.number.isRequired,
+  onCountChange: PropTypes.func.isRequired,
+}
+
+const IncDec = () => {
+  const [count, setCount] = useState(0);
+
+  const onCountChange = (newCount) => {
+    setCount(newCount);
+  }
 
   return (
     <div className="container form-group">
-      <h1>{count}</h1>
-      <Button onClick={onIncClick}>Inc</Button>
-      <Button onClick={onDecClick}>Dec</Button>
+      <DisplayCounter count={count} />
+      <CountControls count={count} onCountChange={onCountChange} /> 
     </div>
   );
 };
